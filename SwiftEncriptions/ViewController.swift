65e6b3544ae4eb32e29cd67be821d21285da89e5
Shoaib Khan <shoaib.khan@valuelabs.com>
2023-08-07 11:26:07 +0530

    Initial Commit

---
+++ SwiftEncriptions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftEncriptions
+//
+//  Created by Shoaib Khan on 07/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
