53add0e00b5a264a202ac2b12df4472e6ee1c936
Shoaib Khan <shoaib.khan@valuelabs.com>
2023-08-07 11:28:30 +0530

    First Commit to local

---
+++ SwiftEncriptions/File1.swift
@@ -0,0 +1,14 @@
+//
+//  File1.swift
+//  SwiftEncriptions
+//
+//  Created by Shoaib Khan on 07/08/23.
+//
+
+import Foundation
+
+struct Student {
+    
+    var name: String
+    var age: Int
+}
---
+++ SwiftEncriptions/File2.swift
@@ -0,0 +1,19 @@
+//
+//  File2.swift
+//  SwiftEncriptions
+//
+//  Created by Shoaib Khan on 07/08/23.
+//
+
+import Foundation
+
+class Students {
+    
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
