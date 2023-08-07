d3aea409b319f0b5bd2c6863f783146e55f380c7
Shoaib Khan <shoaib.khan@valuelabs.com>
2023-08-07 11:48:53 +0530

    Thrid commit

---
+++ SwiftEncriptions/HigherOrderFunction.swift
@@ -0,0 +1,14 @@
+//
+//  HigherOrderFunction.swift
+//  SwiftEncriptions
+//
+//  Created by Shoaib Khan on 07/08/23.
+//
+
+import Foundation
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+let arrStr = arr.map({ String($0) })
+
+
